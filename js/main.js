45177c5ff13a1071d8497125bb2ef13ed9f0e1b6
Elronic3 <kuvarzin3229@gmail.com>
2025-07-01 04:01:08 +0300

    fixed burger, photos 1.2.4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-// js/menu.js
-
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector(".header");
   const burgerBtn = header.querySelector(".burger-btn");
 
   burgerBtn.addEventListener("click", function () {
-    // Переключаем класс menu-open у .header
     header.classList.toggle("menu-open");
   });
 });
