7b5df9c42527cee1d670aadc16b2384741310037
Elronic3 <kuvarzin3229@gmail.com>
2025-07-10 00:30:13 +0300

    added modal 1.3.0

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
     header.classList.toggle("menu-open");
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("callbackModal");
+  const openBtns = document.querySelectorAll(".open-modal");
+  const closeBtn = modal.querySelector(".modal__close");
+  const overlay = modal.querySelector(".modal__overlay");
+
+  openBtns.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      modal.classList.add("active");
+    })
+  );
+
+  closeBtn.addEventListener("click", () => modal.classList.remove("active"));
+  overlay.addEventListener("click", () => modal.classList.remove("active"));
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") modal.classList.remove("active");
+  });
+});
